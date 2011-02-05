f079e4ca6399d9b6d51ab96de991243cdae49b28
Andre Santee <andre.santee@gmail.com>
2011-02-05 13:52:39 -0300

    Sprites recortados exibem dimensao padrao corretamente. Novo metodo: getFrameSize

--- jeraGame/src/br/com/jera/graphic/Sprite.java
+++ jeraGame/src/br/com/jera/graphic/Sprite.java
@@ -9,14 +9,21 @@ import br.com.jera.graphic.Math.Vertex;
 
 public class Sprite {
 	
+	int rows, columns;
+	VertexArray[] vertexArrays;
+	private Texture texture;
+	private Vector2 frameSize = new Vector2();
+	private GraphicDevice graphicDevice;
+
 	public Sprite(GraphicDevice graphicDevice, int resourceId, int columns, int rows) {
-		//this.graphicDevice = graphicDevice;
+		this.graphicDevice = graphicDevice;
 		texture = graphicDevice.createStaticTexture(resourceId);
 		this.rows = rows;
 		this.columns = columns;
 		
 		GridCutter cutter = new GridCutter(columns, rows);
 		Rectangle2D[] rects = cutter.rects;
+		frameSize = rects[0].size.multiply(texture.getBitmapSize());
 		
 		vertexArrays = new VertexArray[rects.length];
 		for (int t=0; t<rects.length; t++) {
@@ -45,13 +52,33 @@ public class Sprite {
 	public Vector2 getBitmapSize() {
 		return texture.getBitmapSize();
 	}
+	
+	public Vector2 getFrameSize() {
+		return frameSize;
+	}
+	
+	public void draw(Vector2 pos, float angle, Vector2 normalizedOrigin) {
+		draw(pos, frameSize, angle, normalizedOrigin, 0);
+	}
+
+	public void draw(Vector2 pos, Vector2 normalizedOrigin) {
+		draw(pos, frameSize, 0, normalizedOrigin, 0);
+	}
+
+	public void draw(Vector2 pos, float angle, Vector2 normalizedOrigin, int frame) {
+		draw(pos, frameSize, angle, normalizedOrigin, frame);
+	}
+
+	public void draw(Vector2 pos, Vector2 normalizedOrigin, int frame) {
+		draw(pos, frameSize, 0, normalizedOrigin, frame);
+	}
 
 	public void draw(Vector2 pos, Vector2 size, float angle, Vector2 normalizedOrigin, int frame) {
 		assert(frame < getNumFrames() && frame >= 0);
 
 		texture.bindTexture();
 
-		Vector2 bitmapSize = getBitmapSize();
+		Vector2 bitmapSize = frameSize;
 		Vector2 originOffset = Math.computeSpriteOriginOffset(bitmapSize, normalizedOrigin);
 		originOffset = originOffset.add(new Vector2(0.375f,0.375f));
 		float[] vertices = {
@@ -61,13 +88,10 @@ public class Sprite {
 				originOffset.x, size.y+originOffset.y, 0
 		};
 		vertexArrays[frame].setVertices(vertices);
-		vertexArrays[frame].drawGeometry(new Vector3(pos, 0), new Vector3(0,0,angle),
+		vertexArrays[frame].drawGeometry(
+				new Vector3(pos.add(graphicDevice.getScreenSize().multiply(-0.5f)), 0),
+				new Vector3(0,0,angle),
 				new Vector3(new Vector2(1,-1), 1));
 		texture.unbindTexture();
 	}
-
-	int rows, columns;
-	VertexArray[] vertexArrays;
-	private Texture texture;
-	//private GraphicDevice graphicDevice;
 }
