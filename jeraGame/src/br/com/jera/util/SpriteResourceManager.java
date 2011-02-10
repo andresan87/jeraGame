a720195be913a7dfb59aa2ea34cbe21d630cd83b
Andre Santee <andre.santee@gmail.com>
2011-02-10 16:56:05 -0300

    Novo objeto para ajudar no gerenciamento de sprites

---
+++ jeraGame/src/br/com/jera/util/SpriteResourceManager.java
@@ -0,0 +1,29 @@
+package br.com.jera.util;
+
+import java.util.HashMap;
+
+import br.com.jera.graphic.GraphicDevice;
+import br.com.jera.graphic.Sprite;
+
+public class SpriteResourceManager {
+
+	private HashMap<Integer, Sprite> map = new HashMap<Integer, Sprite>();
+	private GraphicDevice device;
+
+	public SpriteResourceManager(GraphicDevice device) {
+		this.device = device;
+	}
+	
+	public GraphicDevice getGraphicDevice() {
+		return device;
+	}
+
+	public void loadResource(final int id, final int columns, final int rows) {
+		Sprite sprite = new Sprite(device, id, columns, rows);
+		map.put(new Integer(id), sprite);
+	}
+	
+	public Sprite getSprite(final int id) {
+		return map.get(new Integer(id));
+	}
+}
