c8c60cb90d745acb5f74d1a429ddd66d20a3303f
Andre Santee <andre.santee@gmail.com>
2011-02-16 17:51:07 -0300

    Novos testes com Rectangle2D e SortedDisplayableEntityList movido para o projeto dos vikings por ser muito especifico a jogos 2D

--- jeraGameTest/src/br/com/jera/graphic/Rectangle2DTest.java
+++ jeraGameTest/src/br/com/jera/graphic/Rectangle2DTest.java
@@ -11,7 +11,7 @@ public class Rectangle2DTest extends AndroidTestCase {
 		Vector2 m = rect.getCenter();
 		assertEquals("x should match", 40.0f, m.x);
 		assertEquals("y should match", 80.0f, m.y);
-		
+
 		rect = new Rectangle2D(new Vector2(0, 0), new Vector2(100, 100), new Vector2(0.4f, 0.5f));
 		assertEquals("pos.x should match", -40.0f, rect.pos.x);
 		assertEquals("pos.y should match", -50.0f, rect.pos.y);
@@ -23,5 +23,18 @@ public class Rectangle2DTest extends AndroidTestCase {
 		assertEquals("pos.y should match", -60.0f, rect.pos.y);
 		assertEquals("pos.x should match", 100.0f, rect.size.x);
 		assertEquals("pos.y should match", 100.0f, rect.size.y);
+
+		assertEquals("min.x should match", -30.0f, rect.getMin().x);
+		assertEquals("min.y should match", -60.0f, rect.getMin().y);
+		assertEquals("max.x should match", 70.0f, rect.getMax().x);
+		assertEquals("max.y should match", 40.0f, rect.getMax().y);
+
+		Rectangle2D rect0 = new Rectangle2D(new Vector2(30, 60), new Vector2(70, 10));
+		Rectangle2D rect1 = new Rectangle2D(new Vector2(0, 0), new Vector2(70, 70));
+		assertEquals("return value should match", true, rect0.isIntersecting(rect1));
+		
+		rect0 = new Rectangle2D(new Vector2(30, 60), new Vector2(70, 10));
+		rect1 = new Rectangle2D(new Vector2(0, 0), new Vector2(10, 10));
+		assertEquals("return value should match", false, rect0.isIntersecting(rect1));
 	}
 }
